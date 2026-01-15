9c817ae04c41379221adaed9cbb7bdc4ec72dc80
Veronika Karpenko <veronika.karpenko@intel.com>
2026-01-15 10:33:27 +0000

    4-6

---
+++ asm_training/asm_functions.asm
@@ -0,0 +1,122 @@
+%ifdef LINUX
+;;; macro to declare global symbols
+;;;  - name : symbol name
+;;;  - type : function or data
+%define MKGLOBAL(name,type) global name %+ : %+ type
+%else
+;;; macro to declare global symbols
+;;;  - name : symbol name
+;;;  - type : function or data
+%define MKGLOBAL(name,type) global name
+%endif
+
+section .text
+; uint64_t asm_sum_by_value(uint64_t x, uint64_t y);
+;
+; Add two 64-bit values and return result (value variant)
+MKGLOBAL(asm_sum_by_value,function)
+asm_sum_by_value:
+        mov rax, rdi
+        add rax, rsi
+        ret
+
+; void asm_sum_by_ref(uint64_t *x, uint64_t *y, uint64_t *ret);
+;
+; Add two 64-bit values and return result (reference variant)
+MKGLOBAL(asm_sum_by_ref,function)
+asm_sum_by_ref:
+        mov rax, [rdi]
+        add rax, [rsi]
+        mov [rdx], rax
+        ret
+
+; uint64_t asm_mul(uint32_t x, uint32_t y);
+;
+; Multiply 32-bit values and return result
+MKGLOBAL(asm_mul,function)
+asm_mul:
+        mov eax, edi
+        mul esi ; result in edx:eax (rdx)
+        shl rdx, 32
+        or rax, rdx
+        ret
+
+;uint32_t asm_sum8(uint16_t a1, uint16_t a2, uint16_t a3, uint16_t a4,
+;                  uint16_t a5, uint16_t a6, uint16_t a7, uint16_t a8);
+;
+; Adds eight 16-bit values and return the 32-bit result
+MKGLOBAL(asm_sum8,function)
+asm_sum8:
+        mov eax, edi
+        add eax, esi
+        add eax, edx
+        add eax, ecx
+        add eax, r8d
+        add eax, r9d
+
+        add eax, r10d ; stack
+        add eax, r11d ; stack
+        ret
+
+; void asm_sum_array(uint32_t x[4], uint32_t y[4], uint32_t ret[4]);
+;
+; Add array of 32-bit values and return result
+MKGLOBAL(asm_sum_array,function)
+asm_sum_array:
+        mov eax, [rdi+0]
+        add eax, [rsi+0]
+        mov [rdx+0], eax
+
+        mov eax, [rdi+4] ; is rdi[4] or [rdi+4*1] better?
+        add eax, [rsi+4]
+        mov [rdx+4], eax
+
+        mov eax, [rdi+8]
+        add eax, [rsi+8]
+        mov [rdx+8], eax
+
+        mov eax, [rdi+12]
+        add eax, [rsi+12]
+        mov [rdx+12], eax
+        
+        ret
+
+; uint16_t asm_min_array(uint16_t x[4]);
+;
+; Find the minimum value of an array of four 16-bit values
+MKGLOBAL(asm_min_array,function)
+asm_min_array:
+        mov ax, [rdi+0]
+
+        cmp ax, [rdi+2]
+        jb lessthan
+        mov ax, [rdi+2]
+
+        lessthan:
+        cmp ax, [rdi+4]
+        jb lessthan2
+        mov ax, [rdi+4]
+
+        lessthan2:
+        cmp ax, [rdi+6]
+        jb lessthan3
+        mov ax, [rdi+6] ; conditional moves
+
+        lessthan3:
+        ret
+
+; void memcpy_bytes(void *src, void *dst, uint32_t num_bytes);
+;
+; Copy "num_bytes" number of bytes from source to destination
+MKGLOBAL(memcpy_bytes,function)
+memcpy_bytes:
+
+        ret
+
+; void memcpy_bits(void *src, void *dst, uint32_t num_bits);
+;
+; Copy "num_bits" number of bits from source to destination
+MKGLOBAL(memcpy_bits,function)
+memcpy_bits:
+
+        ret
